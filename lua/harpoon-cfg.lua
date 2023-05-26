88863c3cd5bc955cc925c7282df4d67d7140da93
Joris Guisson <joris.guisson@luminex.be>
2023-05-26 08:28:16 +0000

    Add CR after harpoon keybinds

--- lua/harpoon-cfg.lua
+++ lua/harpoon-cfg.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('n', '<leader>m', ':lua require("harpoon.mark").add_file()', {})
-vim.keymap.set('n', '<leader>h', ':lua require("harpoon.ui").toggle_quick_menu()', {})
+vim.keymap.set('n', '<leader>m', ':lua require("harpoon.mark").add_file()<CR>', {})
+vim.keymap.set('n', '<leader>h', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', {})
